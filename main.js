47913219462cf2699464c044c185b829cb2cc8c2
santisarapinas <santitouch@gmail.com>
2025-04-04 13:13:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 // Requirements: An image and its depth map
 // Based on: https://github.com/d3adrabbit/ScanningEffectWithDepthMap
 
-import { mat4 } from 'gl-matrix';
 import vertexShaderWGSL from './shaders/vertex.wgsl';
 import fragmentShaderWGSL from './shaders/fragment.wgsl';
 
